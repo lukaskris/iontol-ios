ffbc7f9a884108fd14fafd08b18ab66294636c32
lukaskris12 <lukaskris12@gmail.com>
2026-04-11 21:51:47 +0700

    Initial Commit

---
+++ ION Toll/ION_TollApp.swift
@@ -0,0 +1,17 @@
+//
+//  ION_TollApp.swift
+//  ION Toll
+//
+//  Created by install on 11/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ION_TollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
